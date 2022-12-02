a98b0be2abd8b696417b9ac33a007e122c4d780a
Mohamed Elsharnouby <sharnoby3@gmail.com>
2022-12-02 05:21:33 +0000

    solve day 2

--- day2.go
+++ day2.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func day2() {
-  
+	bytes, _ := os.ReadFile("day2input")
+	lines := strings.Split(string(bytes), "\n")
+
+  score1 := 0
+  for _, line := range lines {
+    score1 += day2score1(line)
+  }
+  fmt.Println(score1)
+
+  score2 := 0
+  for _, line := range lines {
+    score2 += day2score2(line)
+  }
+  fmt.Println(score2)
+}
+
+func day2score1(s string) int {
+  switch s {
+    case "A X": return 3 + 1
+    case "B X": return 0 + 1
+    case "C X": return 6 + 1
+    case "A Y": return 6 + 2
+    case "B Y": return 3 + 2
+    case "C Y": return 0 + 2
+    case "A Z": return 0 + 3
+    case "B Z": return 6 + 3
+    case "C Z": return 3 + 3
+  }
+  panic("shouldn't come here")
+}
+
+func day2score2(s string) int {
+  switch s {
+    case "A X": return 3
+    case "B X": return 1
+    case "C X": return 2
+    case "A Y": return 1 + 3
+    case "B Y": return 2 + 3
+    case "C Y": return 3 + 3
+    case "A Z": return 2 + 6
+    case "B Z": return 3 + 6
+    case "C Z": return 1 + 6
+  }
+  panic("shouldn't come here")
 }
--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-	day1()
+	// day1()
+	day2()
 }
